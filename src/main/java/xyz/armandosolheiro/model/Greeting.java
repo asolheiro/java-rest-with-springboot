4afb017fcbfa9be5c2b68ac67021b58e71fe30ee
Armando Solheiro <avgsolheiro@gmail.com>
2025-03-28 23:03:14 -0300

    feat: greeting endpooint

---
+++ src/main/java/xyz/armandosolheiro/model/Greeting.java
@@ -0,0 +1,5 @@
+package xyz.armandosolheiro.model;
+
+public record Greeting(long id, String content) {
+    
+}
---
+++ src/test/java/xyz/armandosolheiro/StartUp.java
@@ -0,0 +1,13 @@
+package xyz.armandosolheiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
